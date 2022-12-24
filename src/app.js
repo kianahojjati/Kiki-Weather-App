b209bb4e8acb2588844857be435c45c974968a93
Kiana Hojjati <kh20h@yahoo.com>
2022-12-24 18:28:40 +0330

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-night.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+   
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityEelement = document.querySelector("#city");
@@ -86,3 +116,4 @@ let celsiusLLink = document.querySelector("#celsius-link");
 celsiusLLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Tehran");
+displayForecast();
