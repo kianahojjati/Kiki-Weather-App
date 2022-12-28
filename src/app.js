001f5ef8b806b104496ea5292debe764233acad4
Kiana Hojjati <kh20h@yahoo.com>
2022-12-29 01:05:09 +0330

    Removed the imperial conversion

--- src/app.js
+++ src/app.js
@@ -112,31 +112,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLLink.classList.remove("active");
-  fahrenheitLLink.classList.add("active");
-  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLLink.classList.add("active");
-  fahrenheitLLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celciusTemperature);
-}
-let celciusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLLink = document.querySelector("#fahrenheit-link");
-fahrenheitLLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLLink = document.querySelector("#celsius-link");
-celsiusLLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Tehran");
